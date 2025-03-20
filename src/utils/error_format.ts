096aeb13c813b88519bd77a6a604e670426da66a
mapleincode <llkj13579@126.com>
2025-03-20 14:56:08 +0800

    feat: push

--- src/utils/error_format.ts
+++ src/utils/error_format.ts
@@ -2,7 +2,7 @@
  * @Author: maple
  * @Date: 2020-09-22 09:25:53
  * @LastEditors: maple
- * @LastEditTime: 2021-10-22 16:14:20
+ * @LastEditTime: 2025-03-20 14:55:38
  */
 import fs from "fs";
 import path from "path";
@@ -20,11 +20,11 @@ const errorFormat: ErrorFormatFunction = function (errorPath: string, errorCode:
     let mmp: StringToErrorMap;
 
     if (ErrorMap[subPath] !== undefined) {
-        mmp = ErrorMap[errorPath];
+        mmp = ErrorMap[subPath];
     } else {
-        const json = fs.readFileSync(path.join(__dirname, `../../errors${errorPath}.json`), { encoding: 'utf-8'});
+        const json = fs.readFileSync(path.join(__dirname, `../../errors${subPath}.json`), { encoding: 'utf-8'});
         mmp = JSON.parse(json);
-        ErrorMap[errorPath] = mmp;
+        ErrorMap[subPath] = mmp;
     }
 
     let errorMsg = mmp[errorCode.toString()];
