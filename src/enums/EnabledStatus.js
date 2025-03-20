26f825fe3cb57406c6a5aab1e8d79873c65cd6a9
mapleincode <llkj13579@126.com>
2025-03-20 14:46:54 +0800

    feat: push

--- src/enums/EnabledStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EnabledStatus = void 0;
-/* eslint-disable @typescript-eslint/no-unused-vars */
-var EnabledStatus;
-(function (EnabledStatus) {
-    EnabledStatus["enabled"] = "1";
-    EnabledStatus["disabled"] = "0";
-})(EnabledStatus || (exports.EnabledStatus = EnabledStatus = {}));
--- src/enums/RecordStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RecordStatus = void 0;
-var RecordStatus;
-(function (RecordStatus) {
-    RecordStatus[RecordStatus["enabled"] = 1] = "enabled";
-    RecordStatus[RecordStatus["disabled"] = 0] = "disabled";
-})(RecordStatus || (exports.RecordStatus = RecordStatus = {}));
